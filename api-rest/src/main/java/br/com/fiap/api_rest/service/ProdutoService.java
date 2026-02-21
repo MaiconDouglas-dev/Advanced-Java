ba56024a19ace6c73f36bda14cd1011144a8907a
MaiconDouglas-dev <maicon.timot8@gmail.com>
2026-02-20 22:55:13 -0300

    atCriação do CRUD para loja de Churros

---
+++ api-rest/src/main/java/br/com/fiap/api_rest/service/ProdutoService.java
@@ -0,0 +1,39 @@
+package br.com.fiap.api_rest.service;
+
+import br.com.fiap.api_rest.model.Produto;
+import br.com.fiap.api_rest.repository.ProdutoReppository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ProdutoService {
+    @Autowired
+    private ProdutoReppository produtoReppository;
+
+    //CRUD
+    public Produto create(Produto produto) {
+        return produtoReppository.save(produto);
+    }
+
+    public Produto read(UUID id) {
+        Optional<Produto> produto = produtoReppository.findById(id);
+        return produto.orElse(null);
+    }
+
+    public List<Produto> read() {
+        return produtoReppository.findAll();
+    }
+
+    public Produto update(Produto produto) {
+        return  produtoReppository.save(produto);
+    }
+
+    public void delete(UUID id) {
+        produtoReppository.deleteById(id);
+    }
+
+}
