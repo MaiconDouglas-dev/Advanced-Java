ac71e38f9811b05834127f46a088c799a7c095f4
MaiconDouglas-dev <maicon.timot8@gmail.com>
2026-05-06 22:18:06 -0300

    Fork do repositorio

--- api-rest/src/main/java/br/com/fiap/api_rest/dto/ProdutoLista.java
+++
@@ -1,6 +0,0 @@
-package br.com.fiap.api_rest.dto;
-
-import org.springframework.hateoas.Link;
-
-public record ProdutoLista(String nome, Link link) {
-}
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import model.Carro;
-
-public class Main {
-    public static void main(String[] args) {
-        Carro carro = new Carro("Fusca", 1975, "VW");
-        System.out.println(carro.toString());
-    }
-}
