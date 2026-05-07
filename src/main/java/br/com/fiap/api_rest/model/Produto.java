c56e766c66332e2208cd9cb1fd846777ab6a0028
MaiconDouglas-dev <maicon.timot8@gmail.com>
2026-05-06 22:18:36 -0300

    Fork do repositorio

---
+++ src/main/java/br/com/fiap/api_rest/model/Produto.java
@@ -0,0 +1,83 @@
+package br.com.fiap.api_rest.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUTOS")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+    @Column(name = "nome_produto")
+    private String nome;
+    @Column(name = "preco_produto")
+    private BigDecimal preco;
+    @Column(name = "data_validade")
+    private LocalDate expiracao;
+    private Categoria categoria;
+    private int estoque;
+    @ManyToMany(mappedBy = "produtos")
+    private List<Pedido> pedidos;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public LocalDate getExpiracao() {
+        return expiracao;
+    }
+
+    public void setExpiracao(LocalDate expiracao) {
+        this.expiracao = expiracao;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+}
---
+++ src/main/java/br/com/fiap/api_rest/service/ProdutoService.java
@@ -0,0 +1,72 @@
+package br.com.fiap.api_rest.service;
+
+import br.com.fiap.api_rest.dto.ProdutoLista;
+import br.com.fiap.api_rest.dto.ProdutoRequest;
+import br.com.fiap.api_rest.dto.ProdutoResponse;
+import br.com.fiap.api_rest.mapper.ProdutoMapper;
+import br.com.fiap.api_rest.model.Produto;
+import br.com.fiap.api_rest.repository.ProdutoRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class ProdutoService {
+    private final ProdutoRepository produtoRepository;
+    private final ProdutoMapper produtoMapper;
+
+    @Autowired
+    public ProdutoService(ProdutoRepository produtoRepository, ProdutoMapper produtoMapper) {
+        this.produtoRepository = produtoRepository;
+        this.produtoMapper = produtoMapper;
+    }
+
+    // CRUD
+    public Produto create(ProdutoRequest produtoRequest) {
+        Produto produto = new Produto();
+        BeanUtils.copyProperties(produtoRequest, produto);
+        return produtoRepository.save(produto);
+    }
+
+    public ProdutoResponse read(UUID id) {
+        Optional<Produto> produto =  produtoRepository.findById(id);
+        if (produto.isEmpty()) {
+            return null;
+        }
+        return produtoMapper.produtoToResponse(produto.get());
+    }
+
+    // Page, Pageable
+    public Page<ProdutoLista> read(Pageable pageable) {
+        return produtoRepository
+                .findAll(pageable)
+                .map(produtoMapper::produtoToProdutoLista);
+    }
+
+    /*
+    // Exemplo usando for em vez de stream
+    public List<ProdutoResponse> read() {
+        List<Produto> produtos = produtoRepository.findAll();
+        List<ProdutoResponse> produtosResponse = new ArrayList<>();
+        for (Produto produto : produtos) {
+            produtosResponse.add(produtoMapper.produtoToResponse(produto));
+        }
+        return produtosResponse;
+    }
+     */
+
+    public Produto update(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public void delete(UUID id) {
+        produtoRepository.deleteById(id);
+    }
+}
